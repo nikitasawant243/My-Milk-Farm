f815e295f149f12ad2194f75a976234aba76461f
Nikita Sawant <nikitasawant243@gmail.com>
2025-07-27 11:01:49 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Fresh Dairy Products Here</h2>
+        <p>Choose from a fresh range of milk, paneer, curd, and more — all sourced from trusted farms and delivered to your doorstep. Our mission is to bring purity, taste, and nutrition to your table, one dairy delight at a time.</p>
+        <button>Order Now</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
